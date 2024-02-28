d54caf3666eb95c3775d9fd106352d822fe5745e
WatkornFeng <watcharakornk2022@gmail.com>
2024-02-29 02:24:23 +0700

    Customize TextField

--- src/features/properties/CreatePropertyForm.tsx
+++ src/features/properties/CreatePropertyForm.tsx
@@ -1,29 +1,72 @@
-import { Box } from "@mui/material";
-import { Form, SubmitHandler, useForm, Controller } from "react-hook-form";
+import { Box, TextField } from "@mui/material";
+import { SubmitHandler, useForm, Controller } from "react-hook-form";
 import toast from "react-hot-toast";
-enum GenderEnum {
-  female = "female",
-  male = "male",
-  other = "other",
-}
 
 interface IFormInput {
-  firstName: string;
-  gender: GenderEnum;
+  propertyName: string;
 }
 function CreatePropertyForm() {
-  const { register, handleSubmit } = useForm<IFormInput>();
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<IFormInput>({
+    defaultValues: {
+      propertyName: "",
+    },
+  });
+  console.log(errors);
   const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <label>First Name</label>
-      <input {...register("firstName")} />
-      <label>Gender Selection</label>
-      <select {...register("gender")}>
-        <option value="female">female</option>
-        <option value="male">male</option>
-        <option value="other">other</option>
-      </select>
+      <label htmlFor="property">Property name</label>
+      <Controller
+        name="propertyName"
+        control={control}
+        rules={{
+          required: {
+            value: true,
+            message: "This field required",
+          },
+          maxLength: {
+            value: 40,
+            message: "Property name must have less or equal than 40 characters",
+          },
+          minLength: {
+            value: 10,
+            message: "Property name must have more or equal than 10 characters",
+          },
+        }}
+        render={({ field }) => (
+          <TextField
+            {...field}
+            id="property"
+            placeholder="Your property name."
+            variant="outlined"
+            sx={{
+              "& .MuiOutlinedInput-root": {
+                color: "primary.constrast",
+                fontWeight: "bold",
+
+                "& .MuiOutlinedInput-notchedOutline": {
+                  borderColor: "primary.constrast",
+                  borderWidth: "0.2rem",
+                },
+                "&.Mui-focused": {
+                  "& .MuiOutlinedInput-notchedOutline": {
+                    borderColor: "lightgreen",
+                    borderWidth: "3px",
+                  },
+                },
+              },
+            }}
+            error={!!errors.propertyName}
+            helperText={errors.propertyName && errors.propertyName.message}
+          />
+        )}
+      />
+
       <input type="submit" />
     </form>
   );
